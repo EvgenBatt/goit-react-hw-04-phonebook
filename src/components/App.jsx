184dff71693e54462ecb978f863697426210e24e
EvgenBatt <evgenmgid@gmail.com>
2023-07-22 15:08:12 +0300

    Fix Task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,23 +6,17 @@ import { Title, SubTitle, EmptyContact } from './App.styled';
 import toast, { Toaster } from 'react-hot-toast';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    return (
+  const [contacts, setContacts] = useState(
+    () =>
       JSON.parse(localStorage.getItem('users')) ?? [
         { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
         { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
         { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
         { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
       ]
-    );
-  });
+  );
 
   const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    const localData = localStorage.getItem('users');
-    if (localData) setContacts(JSON.parse(localData));
-  }, []);
 
   useEffect(() => {
     localStorage.setItem('users', JSON.stringify(contacts));
@@ -31,10 +25,6 @@ export const App = () => {
   const removeContact = id => {
     toast.error('Delete user successfully');
     setContacts(prev => prev.filter(contact => contact.id !== id));
-    localStorage.setItem(
-      'users',
-      JSON.stringify(contacts.filter(contact => contact.id !== id))
-    );
   };
 
   const addContact = contact => {
@@ -49,8 +39,6 @@ export const App = () => {
     }
 
     setContacts(prevContacts => [contact, ...prevContacts]);
-
-    localStorage.setItem('users', JSON.stringify([contact, ...contacts]));
   };
 
   const changeFilter = e => {
